de03e3198da370569c76cce2c9848c2d7db16379
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-14 17:18:37 +0000

    conversion or datatypes

---
+++ 03_conversionoperation.js
@@ -0,0 +1,21 @@
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof BooleanisLoggedIn)
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

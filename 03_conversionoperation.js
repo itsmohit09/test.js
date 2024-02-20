32a5a9bd6e09c57f6d1942c7d4c347b449bccb71
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-20 04:36:44 +0000

    conversionoperation

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -10,12 +10,41 @@
 let isLoggedIn = 1;
 
 let BooleanisLoggedIn = Boolean(isLoggedIn)
-console.log(typeof BooleanisLoggedIn)
+// console.log(typeof BooleanisLoggedIn)
 
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// **************operations**************
+
+let value = 3;
+
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "mohit"
+let str2 = " kumar"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log("2" + 1)
+// console.log(45 + "1")
+// console.log("1" + 1 + 1)
+// console.log(1+1+"1")
+
+let x = 3 
+let y = x++
+let newValue = x
+let newValueofY = y
+
+console.log(`x:${x} , y:${y}`)
+console.log(y)
+
+console.log(newValue)
+console.log(newValueofY)

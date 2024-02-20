e418c277f486d0c3c65fa4fb7ec5fdd0f3151685
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-20 04:36:22 +0000

    datatype and summary

---
+++ datatypes_summary.js
@@ -0,0 +1,37 @@
+// primitives data types
+
+//  7 types : - string, number, boolean, null, undefined, symbol, bigInt
+
+ const score = 11
+const scoreValue = 101.2
+
+const loggedIn = false
+
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 11111111111111111111n
+
+console.log(id === anotherId)
+
+
+// Referance data types 
+// Arrays, Objects, functions
+
+const myArrays = [ "mohit" , "Rohit" , "Ankit"]  // Arrays
+
+let myObj = {
+    name: "mohit",
+    age: 20,
+    height: 6,
+}
+
+const myFunction = function (){
+    console.log("Hello World")
+
+}
+
+console.log(typeof bigNumber)

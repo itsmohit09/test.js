60cc163648e56491bd902427f2b01a9fa7b41dd3
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-22 06:20:25 +0000

    datesinjs

---
+++ datesinjs.js
@@ -0,0 +1,27 @@
+// dates+++++
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date (2024, 1, 22)
+// let myCreatedDate = new Date (2024, 1, 22, 6, 6)
+let myCreatedDate = new Date ("2024-02-22")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// let myTimeStamp = new Time()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
+console.log(newDate.getFullYear())
+console.log(newDate.getUTCDate())

81591307925c88ca8b5f95822c43d26e5a6d4e28
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-26 15:51:58 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,21 @@
+// const tinderUser = new Object()  // single tang object
+const tinderUser = {}  // non single tang
+
+tinderUser.id ="123abc"
+tinderUser.name = "mohit"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    fullName: {
+         userFullName:{
+            firstName: "mohit",
+            lastName: "kumar",
+         }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.firstName)
+
+ 

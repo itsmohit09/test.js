c81e71c695bde59ac89d7540c7133c909a22311e
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-28 04:01:03 +0000

    objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,4 +23,28 @@ const regularUser = {
 
 //  const obj3 = Object.assign({} , obj1, obj2)
 const obj3 = {...obj1, ...obj2}
- console.log(obj3)
+//  console.log(obj3)
+
+const user = [
+    {
+        id:1,
+        email:"mohit@gmail.com"
+    },
+    {
+        id:1,
+        email:"mohit@gmail.com"
+    },
+    {
+        id:1,
+        email:"mohit@gmail.com"
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));

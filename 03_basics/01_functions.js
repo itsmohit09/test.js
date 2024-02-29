b671b9d1ced35f041b8abfdabef00c39439c603d
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-02-29 04:32:29 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,10 +24,41 @@ const result = addTwoNumbers(23 , 23)
 
 function userLoggedIn (userName = "sam") {
     if(!userName){
-        console.log("please enter a username");
+        // console.log("please enter a username");
         return;
     }
     return `${userName} Just Logged In`
 } 
 
-console.log(userLoggedIn("mohit"))
+// console.log(userLoggedIn("mohit"))
+
+
+// *************************new*************************
+
+function calculateCartPrize (val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrize(200, 300, 600, 1000, 2100));
+
+const user = {
+    userName : "mohit",
+    price : 199,
+}
+
+function myObject (anyobject) {
+    console.log(`username is ${anyobject.userName} and Price is ${anyobject.Price}`)
+}
+
+// myObject(user)
+myObject({
+    userName : "Mohit",
+    Price : 399
+})
+
+const myNewArray = [100, 200, 300 ,500]
+function returnThirdValue (getArray){
+    return getArray[2]
+}
+
+console.log(returnThirdValue(myNewArray));

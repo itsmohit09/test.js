4671ba10092453e73ee67ee267be1abfa616ce57
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-03-05 04:29:37 +0000

    controlflow3

---
+++ control_flow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = "h@hitesh.ai"
+
+if(userEmail){
+    console.log("Got user Email");
+}else{
+    console.log("dont have user Email");
+}
+
+// falsy Values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy value
+
+// "0", "false", " ", [], {}, function(){}

b457c049f87556a9cd3e450b75ea6476ef3204c9
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-03-07 17:27:29 +0000

    truthy.js

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -1,9 +1,9 @@
-const userEmail = "h@hitesh.ai"
+const userEmail = ""
 
 if(userEmail){
     console.log("Got user Email");
 }else{
-    console.log("dont have user Email");
+    console.log("don't have user Email");
 }
 
 // falsy Values
@@ -13,3 +13,12 @@ if(userEmail){
 // truthy value
 
 // "0", "false", " ", [], {}, function(){}
+
+if(userEmail.length === 0){
+    console.log("Array is empty")
+}
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty");
+}

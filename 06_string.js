71a6d1e47004bc702a5b573b7028dacc3162da6f
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-07-24 07:44:25 +0000

    strings

--- 06_string.js
+++ 06_string.js
@@ -1,37 +1,72 @@
-const name = "hitesh"
-const repoCount = 50;
+// const name = "hitesh"
+// const repoCount = 50;
 
-// console.log(name + repoCount  +  " value")
+// // console.log(name + repoCount  +  " value")
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+// // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String ("PubgMobile-bgmi")
-// console.log(gameName)
-// console.log(gameName[0])
-// console.log(gameName.__proto__)
+// const gameName = new String ("PubgMobile-bgmi")
+// // console.log(gameName)
+// // console.log(gameName[0])
+// // console.log(gameName.__proto__)
 
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// // console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('g'))
+// const newString = gameName.substring(1,3)
+// console.log(newString)
+// const anotherString = gameName.slice(-11 , 4)
+// console.log(anotherString)
+
+// const newStringOne = "   mohit   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+
+// // console.log(gameName.indexOf('M'))
+
+// // console.log(gameName.charAt(7))
+// // const newStringModified = gameName.substring(2, 5)
+// // console.log(newStringModified)
+
+// const url = "https://hitesh.com/Hitesh%20choudhary"
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+// console.log(gameName.split('-'))
+
+
+let name = "hitesh"
+let repoCount = 40;
+
+// console.log(name + repoCount );
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-url')
+
+// console.log(gameName.__proto__);
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-// console.log(gameName.charAt(2))
-console.log(gameName.indexOf('g'))
-const newString = gameName.substring(1,3)
-console.log(newString)
-const anotherString = gameName.slice(-11 , 4)
-console.log(anotherString)
 
-const newStringOne = "   mohit   "
+// console.log(gameName.charAt(4))
+// console.log(gameName.indexOf("t"))
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(2, 4)
+// console.log(anotherString)
+
+const newStringOne = "    hitesh    "
 console.log(newStringOne)
 console.log(newStringOne.trim())
 
 
-// console.log(gameName.indexOf('M'))
+const url = "https://hitesh.com/hitesh%20chaudhary"
 
-// console.log(gameName.charAt(7))
-// const newStringModified = gameName.substring(2, 5)
-// console.log(newStringModified)
-
-const url = "https://hitesh.com/Hitesh%20choudhary"
 console.log(url.replace('%20', '-'))
+console.log(url.includes('hitesh'))
 
-console.log(url.includes('sundar'))
-console.log(gameName.split('-'))
+console.log(gameName.split(' '))

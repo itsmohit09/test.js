d13b6d58e6f166006d9a67930a14663f98bdca70
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-07-25 04:18:44 +0000

    arrays.js1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,36 +1,83 @@
-// Arrays
+// // Arrays
 
-const myArr = [1, 2, 3, 4, 5, 6]
-const myHeros = ["saktiman", "naagraj"]
-const myArr2 = new Array(1, 3, 4, 5, 6)
+// const myArr = [1, 2, 3, 4, 5, 6]
+// const myHeros = ["saktiman", "naagraj"]
+// const myArr2 = new Array(1, 3, 4, 5, 6)
 
 // console.log(myArr[3])
 
 
-// Arrays method
+// // Arrays method
 
-// myArr.push(7)
-// myArr.push(12)
-// myArr.pop()
+// // myArr.push(7)
+// // myArr.push(12)
+// // myArr.pop()
 
-// myArr.unshift(23)
+// // myArr.unshift(23)
+// // myArr.shift()
+
+// // console.log(myArr.includes(5))
+// // console.log(myArr.indexOf(5))
+
+// // const NewArr = [11, 21, 31, 41, 51, 61]
+// // console.log(myArr);
+// // console.log(typeof (NewArr.join()));
+
+
+// console.log("A ", myArr)
+
+// const myn1 = myArr.slice(1,3) 
+// console.log(myn1); 
+ 
+// // note :-  
+
+
+// console.log("B ", myArr);   
+ 
+
+
+
+
+// const myn2 = myArr.splice(1,3) 
+// console.log("C ", myArr) 
+// console.log(myn2);  
+ 
+
+
+
+const myArr = [1,2,3,4,5,6]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr[2])
+
+// console.log(myArr2.length)
+
+// console.log(myHeros[0])
+
+//  myArr.push(10)
+//  myArr.push(20)
+//  myArr.pop()
+
+// myArr.unshift(6)
 // myArr.shift()
 
 // console.log(myArr.includes(5))
-// console.log(myArr.indexOf(5))
+// console.log(myArr.indexOf(10))
 
-// const NewArr = [11, 21, 31, 41, 51, 61]
-// console.log(myArr);
-// console.log(typeof (NewArr.join()));
+const newArr = myArr.join()
 
+// console.log(myArr)
+// console.log(typeof newArr)
 
-console.log("A ", myArr)
+console.log("A " ,myArr)
 
-const myn1 = myArr.slice(1,3)
-console.log(myn1);
+const newArray = myArr.slice(1, 3)
+console.log(newArray)
 
-console.log("B ", myArr);
+console.log("B", myArr)
 
-const myn2 = myArr.splice(1,3)
-console.log("C ", myArr)
-console.log(myn2);
+const newArray2 = myArr.splice(1,3)
+console.log("C" , myArr)
+console.log(newArray2)

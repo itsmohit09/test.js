654aae6f278e3ce83fe85472623abfaedea3a271
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-07-25 05:30:41 +0000

    arrays.js2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,26 +1,52 @@
+// const marvelHeros = ["Thor", "Ironman", "Spiderman"];
+// const dc = ["superman", "flash", "Batman"];
+
+// // marvelHeros.push(dc)
+// // console.log(marvelHeros[3][2]);
+
+// // const allHeros = marvelHeros.concat(dc)
+// // console.log(allHeros)
+
+// // const allNewHeros = [...marvelHeros, ...dc]
+// // console.log(allNewHeros);
+
+// const anotherArray = [1,2,3,[4,5,6],7,8,[23,34,45,[45,56]]]
+
+// // const newAnotherArray = anotherArray.flat(Infinity)
+// // console.log(newAnotherArray)
+
+// // console.log(Array.isArray("Hitesh"))
+// // console.log(Array.from("Hitesh"))
+// // console.log(Array.from({name: "Hitesh"}))   // intersting
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// console.log(Array.of(score1, score2, score3))
+
 const marvelHeros = ["Thor", "Ironman", "Spiderman"];
 const dc = ["superman", "flash", "Batman"];
 
 // marvelHeros.push(dc)
-// console.log(marvelHeros[3][2]);
+// console.log(marvelHeros)
+// console.log(marvelHeros[3][1])
 
 // const allHeros = marvelHeros.concat(dc)
 // console.log(allHeros)
 
-// const allNewHeros = [...marvelHeros, ...dc]
-// console.log(allNewHeros);
+const newHeros = [...marvelHeros, ...dc]
+// console.log(newHeros)
 
-const anotherArray = [1,2,3,[4,5,6],7,8,[23,34,45,[45,56]]]
+const anotherArray = [1,2,4 ,[4,3,6], [5,6,7,[6,7,9,0]]]
+// console.log(anotherArray.flat(Infinity))
 
-// const newAnotherArray = anotherArray.flat(Infinity)
-// console.log(newAnotherArray)
+console.log(Array.isArray("hitesh"))
+console.log(Array.from("hitesh"))
+console.log(Array.from({name : "hitesh"})) // interesting
 
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name: "Hitesh"}))   // intersting
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
 
-let score1 = 100;
-let score2 = 200;
-let score3 = 300;
-
-console.log(Array.of(score1, score2, score3))
+console.log(Array.of(score1,score2,score3))

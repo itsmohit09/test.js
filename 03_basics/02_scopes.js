1b95777da0d2ead1045b2b6d2f940eff59df5874
itsmohit09 <136877394+itsmohit09@users.noreply.github.com>
2024-07-29 07:02:36 +0000

    scope and arrow

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,69 +1,119 @@
 // var c = 300
-let a = 300;
+// let a = 300;
+
+// if (true) {
+//     let a = 10 
+//     const b = 20
+//     // console.log("INNER :", a);
+// }
+
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+// function one(){
+//     const username = "Mohit"
+
+//     function two (){
+//         const website = "Youtube"
+//         // console.log(username)
+//     }
+//     // console.log(website)
+
+//     two()
+// }
+// one()
+
+// function username (){
+//     const name = "mohit kumar"
+
+//     function userEmail () {
+//         const email = "mohit@gamil.com"
+
+//         function userage () {
+//             const age = 20
+//             // console.log(name)
+//             // console.log(email)
+//             // console.log(age)
+//         }
+//         userage()
+        
+//     }
+//     userEmail()
+// }
+// username()
+
+
+// if(true) {
+//     const userName = "Mohit"
+//     if(userName === "Mohit"){
+//         const Website = "youtube"
+//         // console.log(userName + " Website")
+//     }
+//     // console.log(Website)
+// }
+// // console.log(userName)
+
+// // ++++++++++++++++++++++ Intersting ++++++++++++++++++++
+
+// function addone (num){
+//     return num + 1
+// }
+// console.log(addone(5))
+
+
+
+// // console.log(addTwo(5));
+// const addTwo = function(num){
+//     return num + 2
+// }
+// var c = 30;
+
+let a = 300
 
 if (true) {
-    let a = 10 
-    const b = 20
-    // console.log("INNER :", a);
+    let a =  10;
+    const b = 20;
+    console.log(a)
 }
-
 // console.log(a)
 // console.log(b)
 // console.log(c)
 
-function one(){
-    const username = "Mohit"
+function one (){
+    const username = "hitesh"
 
     function two (){
+        const website = "Youtube"
+    }
+    // console.log(username)
+    // console.log(website)
+
+    // two()
+}
+
+one()
+
+if(true) {
+    const username = "hitesh"
+    if(username === "hitesh"){
         const website = "Youtube"
         // console.log(username)
     }
     // console.log(website)
 
-    two()
 }
-one()
+// console.log(username)
 
-function username (){
-    const name = "mohit kumar"
+//++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++
 
-    function userEmail () {
-        const email = "mohit@gamil.com"
-
-        function userage () {
-            const age = 20
-            // console.log(name)
-            // console.log(email)
-            // console.log(age)
-        }
-        userage()
-        
-    }
-    userEmail()
-}
-username()
-
-
-if(true) {
-    const userName = "Mohit"
-    if(userName === "Mohit"){
-        const Website = "youtube"
-        // console.log(userName + " Website")
-    }
-    // console.log(Website)
-}
-// console.log(userName)
-
-// ++++++++++++++++++++++ Intersting ++++++++++++++++++++
-
-function addone (num){
+console.log(addOne(7))
+function addOne (num) {
     return num + 1
 }
-console.log(addone(5))
 
-
-
-// console.log(addTwo(5));
 const addTwo = function(num){
-    return num + 2
+    return num + 1
 }
+console.log(addTwo(5)) 
+
--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,44 +1,85 @@
-const user = {
-    username: "Mohit",
-    price: 999,
+// const user = {
+//     username: "Mohit",
+//     price: 999,
 
-    welcomeMassage: function(){
-        console.log(`${this.username} , Welcome to website `)
-        // console.log(this)
-    }
-}
+//     welcomeMassage: function(){
+//         console.log(`${this.username} , Welcome to website `)
+//         // console.log(this)
+//     }
+// }
 
-// user.welcomeMassage()
-// user.username = "sam"
-// user.welcomeMassage()
+// // user.welcomeMassage()
+// // user.username = "sam"
+// // user.welcomeMassage()
 
-// console.log(this)
+// // console.log(this)
 
 
-// function chai (){
+// // function chai (){
+// //     let user = "mohit"
+// //     console.log(this.user);
+// // }
+// // chai()
+
+// // const chai = function ()  {
+// //     let user = "mohit"
+// //     console.log(this.user);
+// // }
+
+
+// const chai = () => {
 //     let user = "mohit"
-//     console.log(this.user);
+//     // console.log(this);
 // }
 // chai()
 
-// const chai = function ()  {
-//     let user = "mohit"
-//     console.log(this.user);
+// // const addTwo = (num1 , num2) => {
+// //     return num1 + num2
+// // }
+
+// // const addTwo = (num1 , num2) =>  (num1 + num2)
+
+// const addTwo = (num1 , num2) =>  ({username : "hitesh"})
+
+// console.log(addTwo(4 , 5));
+
+// const user = {
+    // username : "hitesh",
+    // prize : 999,
+
+    // welcomeMassage : function(){
+    //     console.log( `${this.username} , welcome to our website`)
+    // }
+
+// }
+// user.welcomeMassage
+
+// const user = {
+
+// function chai (){
+//     let newuser = "hitesh"
+//     console.log(this.newuser)
+
+// }
+// chai()
+
+// const chai = function () {
+//     let newuser = "hitesh"
+//     console.log(this.newuser)
+
 // }
 
+// chai()
 
-const chai = () => {
-    let user = "mohit"
-    // console.log(this);
+// const chai = () => {
+//     let newuser = "hitesh"
+//     console.log(this)
+// }
+
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2 
 }
-chai()
 
-// const addTwo = (num1 , num2) => {
-//     return num1 + num2
-// }
-
-// const addTwo = (num1 , num2) =>  (num1 + num2)
-
-const addTwo = (num1 , num2) =>  ({username : "hitesh"})
-
-console.log(addTwo(4 , 5));
+console.log(addTwo(3, 5))
